c75e716c790a153fb119de1147a0a534f10b709d
anrongjie <601698515@qq.com>
2017-04-24 11:00:43 +0800

    修改背景颜色
    将背景改为红色

--- ceshi/ViewController.m
+++ ceshi/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     [self.view setBackgroundColor:[UIColor greenColor]];
+    
+    [self.view setBackgroundColor:[UIColor redColor]];
 }
 
 - (void)didReceiveMemoryWarning {
